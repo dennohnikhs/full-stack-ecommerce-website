56034607f8c4f04731673051664dae95e0427cee
Rashon Kiptoo <biikiptoo596@gmail.com>
2024-03-29 15:56:24 +0300

    displayed products to the page

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,7 +1,18 @@
+import ProductItem from "../components/products/ProductItem";
+import data from "../components/products/data";
+
 export default function Home() {
   return (
     <main className="text-2xl py-2">
       <div>Latest Products</div>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem
+            key={product._id} // Changed from "product.id" to "product._id"
+            product={product}
+          />
+        ))}
+      </div>
     </main>
   );
 }
---
+++ app/components/products/ProductItem.tsx
@@ -0,0 +1,36 @@
+import { Product } from "@/.next/types/app/lib/models/ProductModel";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+function ProductItem({ product }: { product: Product }) {
+  return (
+    <>
+      <div className="card bg-base-300 shadow-xl mb-4">
+        <figure className="flex flex-col">
+          <Link href={`/product/${product.slug}`}>
+            <Image
+              src={product.image}
+              alt={product.name}
+              width={300}
+              height={300}
+              className="object-cover w-ful h-64"
+            />
+          </Link>
+
+          <div className="card-body">
+            <Link href={`/product/${product._id}`}>
+              <h2 className="card-title  font-normal">{product.name}</h2>
+            </Link>
+            <p className="mb-2">{product.brand}</p>
+            <div className="card-actions justify-between flex items-center">
+              <span className=" text-2xl">{product.price}</span>
+            </div>
+          </div>
+        </figure>
+      </div>
+    </>
+  );
+}
+
+export default ProductItem;
